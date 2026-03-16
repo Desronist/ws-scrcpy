ea21667612e2bfba5152d037e7b297d527efcec1
Desronist <saykoozturkx@gmail.com>
2026-03-16 19:32:36 +0300

    osd

--- src/server/goog-device/services/AgentService.ts
+++ src/server/goog-device/services/AgentService.ts
@@ -141,8 +141,13 @@ export class AgentService implements Service {
         });
     }
 
+
     private handleTunnel(tunnelId: string, targetUrl: string): void {
-        const tunnelServerUrl = this.serverUrl.replace(/^http/, 'ws').replace(/\/$/, '') + '/api/scrcpy-tunnel-provider/' + tunnelId;
+        const urlObj = new URL(this.serverUrl);
+        urlObj.protocol = urlObj.protocol.replace(/^http/, 'ws');
+        urlObj.pathname = '/api/scrcpy-tunnel-provider/' + tunnelId;
+        const tunnelServerUrl = urlObj.toString();
+        
         // Use 127.0.0.1 instead of localhost for better compatibility on Windows
         const localScrcpyUrl = `ws://127.0.0.1:${this.scrcpyPort}${targetUrl || ''}`;
         
